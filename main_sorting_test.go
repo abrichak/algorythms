c50a0c5d9b6a7337afbc9e5aec9ff4efd3072abe
Oleksandr Brichak <brichak.new@gmail.com>
2022-10-27 19:17:24 +0300

    Add benchmarks to get 4 points to improve regression analysis

--- main_sorting_test.go
+++ main_sorting_test.go
@@ -8,21 +8,24 @@ import (
 func BenchmarkSortingBestN(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := prepareSlice(n, bestCase)
-		sortInsertion(&src)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
 	}
 }
 
 func BenchmarkSortingAverageN(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := prepareSlice(n, averageCase)
-		sortInsertion(&src)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
 	}
 }
 
 func BenchmarkSortingWorstN(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := prepareSlice(n, worstCase)
-		sortInsertion(&src)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
 	}
 }
 
@@ -35,27 +38,90 @@ func BenchmarkOnlyPrepareSliceAverageN(b *testing.B) {
 func BenchmarkSortingBest10N(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := prepareSlice(10*n, bestCase)
-		sortInsertion(&src)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
 	}
 }
 
 func BenchmarkSortingAverage10N(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := prepareSlice(10*n, averageCase)
-		sortInsertion(&src)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
 	}
 }
 
 func BenchmarkSortingWorst10N(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		src := prepareSlice(10*n, worstCase)
-		sortInsertion(&src)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
 	}
 }
 
 func BenchmarkOnlyPrepareSliceAverage10N(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = prepareSlice(10*n, averageCase)
+	}
+}
+
+func BenchmarkSortingBest100N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := prepareSlice(100*n, bestCase)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
+	}
+}
+
+func BenchmarkSortingAverage100N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := prepareSlice(100*n, averageCase)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
+	}
+}
+
+func BenchmarkSortingWorst100N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := prepareSlice(100*n, worstCase)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
+	}
+}
+
+func BenchmarkOnlyPrepareSliceAverage100N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = prepareSlice(100*n, averageCase)
+	}
+}
+
+func BenchmarkSortingBest1000N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := prepareSlice(1000*n, bestCase)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
+	}
+}
+
+func BenchmarkSortingAverage1000N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := prepareSlice(1000*n, averageCase)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
+	}
+}
+
+func BenchmarkSortingWorst1000N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		src := prepareSlice(1000*n, worstCase)
+		//sortInsertion(&src)
+		sortMerge(&src, 0, len(src)-1)
+	}
+}
+
+func BenchmarkOnlyPrepareSliceAverage1000N(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = prepareSlice(1000*n, averageCase)
 	}
 }
 
